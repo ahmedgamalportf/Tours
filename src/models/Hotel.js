3becfe6928ea502a42accbc9a4e1ba60cac21b82
Ahmed21gamal <125803560+Ahmed21gamal@users.noreply.github.com>
2026-07-05 16:09:09 +0300

    added hotel model

---
+++ src/models/Hotel.js
@@ -0,0 +1,80 @@
+const mongoose = require ('mongoose');
+
+const hotelSchema = mongoose.Schema({
+
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    description:{
+        type:String,
+        trim:true,
+        required:true
+    },
+
+    country:{
+        type:String,
+        trim:true,
+        required:true
+    },
+
+    address:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    city: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+   location: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+        
+    images: [
+        {
+            type: String,
+        },
+    ],
+
+    roomsAvailable: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0,
+    },
+
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+
+        isActive: {
+        type: Boolean,
+        default: true,
+        },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    stars: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+
+},
+{
+    timstamps:true
+});
+
+const Hotel = mongoose.model('Hotel',hotelSchema);
+module.exports = Hotel;
